b8690e529b4eabccf3341f999e6acc10cf593a91
Raunak-S <raunak.shrestha@gmail.com>
2022-09-27 00:01:50 -0400

    Implement some Chip-8 spec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+
+    let mut mem: [u8;4096] = [0;4096];
+    let mut pc: u16 = 0;
+    let mut I: u16 = 0;
+    let stack: Vec<u16> = vec![];
+    
+
     println!("Hello, world!");
 }
